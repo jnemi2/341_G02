110b7ab4d7d1ce5888fcac936a8f1d8e9b454ce3
bauti <jnemi@fie.undef.edu.ar>
2021-09-27 17:40:50 -0300

    REF - Assignment 2a

--- bingo/utils.py
+++ bingo/utils.py
@@ -2,9 +2,6 @@ import random
 
 
 def generar_carton():
-    card = []
-    options = list(range(100))
-    for num in range(25):
-        card.append(options.pop(random.randint(0, len(options) - 1)))
-        card.sort()
+    card = random.sample(range(100), 25)
+    card.sort()
     return card
